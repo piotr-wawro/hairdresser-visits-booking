648bde956020b81b849d506dabe85d5c86d3e066
Piotr Wawro <44544007+RANDOM-V2N5q@users.noreply.github.com>
2022-11-10 17:00:44 +0100

    Move config to .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import route from "./route/index.js";
-import errorHandlers from "./middleware/errorHandlers/index.js";
-
 import "./config/dotenv.js";
 import "./config/database.js";
 import "./config/email.js";
+
+import express from "express";
+import route from "./route/index.js";
+import errorHandlers from "./middleware/errorHandlers/index.js";
 
 const app = express();
 const port = 3001;
