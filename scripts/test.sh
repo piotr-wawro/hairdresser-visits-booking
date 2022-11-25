04e422f6adfe94968af1251c991dc8e755cc0571
Piotr Wawro <44544007+piotr-wawro@users.noreply.github.com>
2022-11-25 13:13:36 +0100

    Update build scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
-if [ $1 = "backend" ]
+if [[ $1 == "backend" ]] || [[ $1 == "" ]]
     then
     docker-compose -f ./docker/docker-compose.cicd.yaml run --rm test-backend
 fi
 
-if [ $1 = "frontend" ]
+if [[ $1 == "frontend" ]] || [[ $1 == "" ]]
     then
     docker-compose -f ./docker/docker-compose.cicd.yaml run --rm test-frontend
 fi
